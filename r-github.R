2da03a9f9ecb976c1ccd8ad78c8916d7e8358fa9
Chris Boosalis <cboosalis@csustan.edu>
2013-10-02 15:13:35 -0700

    Corrected URL for datafile

--- r-github.R
+++ r-github.R
@@ -1,4 +1,4 @@
-datafilename="https://github.com/cboosalis/csv/blob/master/COMPLETE-FILE.csv"
+datafilename="https://raw.github.com/cboosalis/csv/master/COMPLETE-FILE.csv"
 fluency1=read.csv(datafilename,header=T)
 attach(fluency1)
 aov1 = aov(Fluency~Program,data=fluency1) 
@@ -8,7 +8,7 @@ boxplot(Fluency~Program,data=fluency1)
 title("Independent reading, not choral reading, promotes greater fluency gains.")
 
 aov2 = aov(Fluency~Program*CELDT,data=fluency1) 
-)summary(aov2)            #show the summary table
+summary(aov2)            #show the summary table
 print(model.tables(aov2,"means"),digits=3)
 boxplot(Fluency~Program*CELDT,data=fluency1)
 title("Greater fluency gains depend on CELDT status and reading program.")
